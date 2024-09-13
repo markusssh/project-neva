3e8a9ab8ba8690e419a30a28d7b651e5828df8c5
Mark Semakin <90873852+markusssh@users.noreply.github.com>
2024-09-13 19:20:36 +0300

    lower panel and ui buitification

--- Drawing/color_picker.gd
+++
@@ -1,8 +0,0 @@
-extends ColorPicker
-
-func _ready() -> void:
-	color = %DrawingLine.default_color
-
-#ALERT: doesn't work. Should freeze ui if not hovered
-func _on_mouse_exited() -> void:
-	release_focus()
--- Drawing/drawing_canvas.gd
+++ Drawing/drawing_canvas.gd
@@ -1,6 +1,7 @@
 extends Control
 
 enum PaintingMode {
+	PIPETTE,
 	BRUSH,
 	ERASER,
 	BUCKET,
@@ -11,20 +12,54 @@ enum PaintingMode {
 	FULL_CIRCLE
 }
 
+enum BrushSize {
+	XS,
+	S,
+	M,
+	L,
+	XL
+}
+
+const DEFAULT_SIZE: BrushSize = BrushSize.M
+const DEFAULT_COLOR: Color = Color.DARK_MAGENTA
+const HISTORY_MAX_SIZE: int = 40
+
 @export var drawing_line: Line2D
 @export var paint_viewport: SubViewport
 @export var painted_image: TextureRect
-@export var background: Panel
+@export var background: ColorRect
 @export var erase_mask_line: Line2D
 @export var erase_mask_viewport: SubViewport
-@export var drawing_background: StyleBoxFlat
+@export var color_picker: ColorPicker
 
 var e: float = 1
 var mouse_pos: Vector2
 var mode: PaintingMode = PaintingMode.BRUSH 
 var canv_size: Vector2i
+var history_scroll_idx: int = 1
+var drawing_history: Array[ImageTexture] = [ImageTexture.new()]
+var brush_size_dict := {
+	BrushSize.XS: 20,
+	BrushSize.S: 40,
+	BrushSize.M: 70,
+	BrushSize.L: 110,
+	BrushSize.XL: 160
+}
 
-@onready var draw_color: Color = drawing_line.default_color
+var brush_size: BrushSize = DEFAULT_SIZE:
+	set(value):
+		brush_size = value
+		drawing_line.width = brush_size_dict[value]
+		erase_mask_line.width =  brush_size_dict[value]
+var drawing_color: Color = DEFAULT_COLOR: 
+	set(value):
+		drawing_color = value
+		color_picker.color = value
+
+func _ready() -> void:
+	color_picker.color = drawing_color
+	drawing_line.width = brush_size_dict[brush_size]
+	drawing_line.width = brush_size_dict[brush_size]
 
 func allign_canvas() -> void:
 	for child in get_children():
@@ -34,14 +69,22 @@ func allign_canvas() -> void:
 	background.pivot_offset = canv_size / 2
 
 func _process(_delta: float) -> void:
+	if Input.is_action_just_released("step_forward"):
+		_history_step_forward()
+	elif Input.is_action_just_released("step_back"):
+		_history_step_back()
 	match mode:
+		PaintingMode.PIPETTE:
+			if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+				set_drawing_canvas_mouse_pos()
+				handle_pipette_pick()
 		PaintingMode.BRUSH:
 			if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-				mouse_pos = paint_viewport.get_mouse_position()
+				set_drawing_canvas_mouse_pos()
 				handle_brush_drawing()
 		PaintingMode.ERASER:
 			if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-				mouse_pos = paint_viewport.get_mouse_position()
+				set_drawing_canvas_mouse_pos()
 				handle_erase()
 
 func _input(event: InputEvent) -> void:
@@ -59,15 +102,32 @@ func _input(event: InputEvent) -> void:
 		PaintingMode.BUCKET:
 			if event is InputEventMouseButton and event.is_released():
 				if event.button_index == MOUSE_BUTTON_LEFT:
-					mouse_pos = paint_viewport.get_mouse_position()
-					if mouse_pos.x < paint_viewport.size.x and mouse_pos.y < paint_viewport.size.y \
-					and mouse_pos.x >= 0 and mouse_pos.y >= 0:
+					set_drawing_canvas_mouse_pos()
+					if inside_drawing_canvas():
 						bake_drawing()
 
+func set_drawing_canvas_mouse_pos() -> void:
+	mouse_pos = paint_viewport.get_mouse_position()
+
+func inside_drawing_canvas() -> bool:
+	return mouse_pos.x < paint_viewport.size.x and mouse_pos.y < paint_viewport.size.y and mouse_pos.x >= 0 and mouse_pos.y >= 0
+
+func handle_pipette_pick() -> void:
+	if inside_drawing_canvas():
+		var p: Color = paint_viewport.get_texture().get_image().get_pixelv(mouse_pos)
+		if p.a != 0:
+			drawing_color = p
+		else: drawing_color = background.color
+
 func handle_brush_drawing() -> void:
+	match mode:
+		PaintingMode.BRUSH:
+			drawing_line.default_color = drawing_color
+		PaintingMode.ERASER:
+			drawing_line.default_color = background.color
+		
 	if drawing_line.points.size() == 0:
-		if mouse_pos.x < paint_viewport.size.x and mouse_pos.y < paint_viewport.size.y \
-		and mouse_pos.x >= 0 and mouse_pos.y >= 0:
+		if inside_drawing_canvas():
 			drawing_line.add_point(mouse_pos)
 			#Adding second point do make visible dot
 			drawing_line.add_point(mouse_pos + Vector2.DOWN)
@@ -86,14 +146,15 @@ func handle_erase() -> void:
 func bake_drawing() -> void:
 	match mode:
 		PaintingMode.ERASER:
-			await RenderingServer.frame_post_draw
-			var erase_mask: Image = erase_mask_viewport.get_texture().get_image()
-			var picture: Image = painted_image.texture.get_image()
-			for row in canv_size.y:
-				for column in canv_size.x:
-					if erase_mask.get_pixel(column, row) == Color.BLACK:
-						picture.set_pixel(column, row, Color(0, 0, 0, 0))
-			painted_image.texture = ImageTexture.create_from_image(picture)
+			if painted_image.texture != null:
+				await RenderingServer.frame_post_draw
+				var erase_mask: Image = erase_mask_viewport.get_texture().get_image()
+				var picture: Image = painted_image.texture.get_image()
+				for row in canv_size.y:
+					for column in canv_size.x:
+						if erase_mask.get_pixel(column, row) == Color.BLACK:
+							picture.set_pixel(column, row, Color(0, 0, 0, 0))
+				painted_image.texture = ImageTexture.create_from_image(picture)
 			drawing_line.clear_points()
 		PaintingMode.BUCKET:
 			var picture: Image
@@ -103,18 +164,40 @@ func bake_drawing() -> void:
 				picture = painted_image.texture.get_image()
 			var replace_color = picture.get_pixelv(mouse_pos)
 			painted_image.texture = ImageTexture.create_from_image(
-				AlgoUtil.FloodFill(picture, mouse_pos, replace_color, draw_color))
+				AlgoUtil.FloodFill(picture, mouse_pos, replace_color, drawing_color))
 		_:
 			painted_image.texture = ImageTexture.create_from_image(paint_viewport.get_texture().get_image())
 			drawing_line.clear_points()
+	_update_history()
 
+func _update_history() -> void:
+	if history_scroll_idx > 1:
+		drawing_history = drawing_history.slice(0, -history_scroll_idx + 1)
+		history_scroll_idx = 1
+	if drawing_history.size() >= HISTORY_MAX_SIZE:
+		drawing_history = drawing_history.slice(1, drawing_history.size())
+	drawing_history.append(painted_image.texture)
+
+func _history_step_back() -> void:
+	if history_scroll_idx + 1 <= drawing_history.size():
+		history_scroll_idx += 1
+		painted_image.texture = drawing_history[-history_scroll_idx]
+
+func _history_step_forward() -> void:
+	if history_scroll_idx >= 2:
+		history_scroll_idx -= 1
+		painted_image.texture = drawing_history[-history_scroll_idx]
+
+#region COLOR PICKER UI
 func _on_color_picker_color_changed(color: Color) -> void:
-	draw_color = color
-	if drawing_line.points.is_empty() and mode != PaintingMode.ERASER:
-		drawing_line.default_color = %ColorPicker.color
+	if drawing_line.points.is_empty():
+		drawing_color = color
+#endregion
 
-func _on_h_slider_value_changed(value: float) -> void:
-	drawing_line.width = %Thickness.value
+#region INSTRUMENT BUTTON GROUP
+func _on_pipette_buttton_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		mode = PaintingMode.PIPETTE
 
 func _on_brush_buttton_toggled(toggled_on: bool) -> void:
 	if toggled_on:
@@ -123,9 +206,6 @@ func _on_brush_buttton_toggled(toggled_on: bool) -> void:
 func _on_eraser_button_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		mode = PaintingMode.ERASER
-		drawing_line.default_color = drawing_background.bg_color
-	else:
-		drawing_line.default_color = %ColorPicker.color
 
 func _on_bucket_button_toggled(toggled_on: bool) -> void:
 	if toggled_on:
@@ -150,3 +230,29 @@ func _on_empty_circle_button_toggled(toggled_on: bool) -> void:
 func _on_full_circle_button_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		mode = PaintingMode.FULL_CIRCLE
+#endregion
+
+#region HISTORY BUTTONS
+func _on_action_back_button_pressed() -> void:
+	_history_step_back()
+
+func _on_action_forward_button_pressed() -> void:
+	_history_step_forward()
+#endregion
+
+#region BRUSH SIZE BUTTONS
+func _on_xs_button_pressed() -> void:
+	brush_size = BrushSize.XS
+
+func _on_s_button_pressed() -> void:
+	brush_size = BrushSize.S
+
+func _on_m_button_pressed() -> void:
+	brush_size = BrushSize.M
+
+func _on_l_button_pressed() -> void:
+	brush_size = BrushSize.L
+
+func _on_xl_button_pressed() -> void:
+	brush_size = BrushSize.XL
+#endregion
